e5dc6ddfcc4675d1f759b094978889c391917a70
Manshal Khatri <khatrimanshal1206@gmail.com>
2023-10-21 17:08:36 +0530

    Reduced App Size

--- app/src/main/java/com/manshal_khatri/codmin/SettingsActivity.kt
+++ app/src/main/java/com/manshal_khatri/codmin/SettingsActivity.kt
@@ -3,44 +3,57 @@ package com.manshal_khatri.codmin
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
-import android.widget.TextView
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
+import androidx.core.view.forEachIndexed
 
-class SettingsActivity : AppCompatActivity() {
-
-    lateinit var developer : TextView
-    lateinit var wireless : TextView
-    lateinit var tetherin : TextView
-    lateinit var deviceInfo : TextView
-  //  lateinit var search : TextView
+class SettingsActivity : AppCompatActivity(),OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-
-        developer = findViewById(R.id.developer)
-        wireless = findViewById(R.id.wireless)
-        tetherin = findViewById(R.id.tetherin)
-        deviceInfo = findViewById(R.id.deviceInfo)
-
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        developer.setOnClickListener { startActivity( Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS))
-            finish()}
-        wireless.setOnClickListener { startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))
-        finish()}
-        tetherin.setOnClickListener { startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
-        finish()}
-        deviceInfo.setOnClickListener { startActivity(Intent(Settings.ACTION_DEVICE_INFO_SETTINGS))
-        finish()}
-
+        findViewById<LinearLayout>(R.id.devTiles).forEachIndexed { index, view ->
+            // todo : Handle the last element (Ad)
+            view.setOnClickListener(this)
+        }
+       /* val mn =if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getSystemService(NfcManager::class.java)
+        }else{
+            getSystemService(NFC_SERVICE) as NfcManager
+        }
+        mn.defaultAdapter?.let {
+            findViewById<View>(R.id.tetherin).apply {
+                isVisible = true
+                setOnClickListener {
+                    startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
+                    finish()
+                }
+            }
+        }*/
         //  FOR FINDING MORE KIND OF SETTINGS
-        // search = findViewById(R.id.search)
-       // search.setOnClickListener { startActivity(Intent(Settings.ACTION_APP_SEARCH_SETTINGS))}
+        // search = findViewById<View>(R.id.search)
+        // search.setOnClickListener { startActivity(Intent(Settings.ACTION_APP_SEARCH_SETTINGS))}
 
     }
+
+    override fun onClick(p0: View?) {
+        goto(when(p0?.id){
+            R.id.developer -> Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS
+            R.id.wireless -> Settings.ACTION_WIRELESS_SETTINGS
+            R.id.deviceInfo -> Settings.ACTION_DEVICE_INFO_SETTINGS
+            else -> null
+        })
+        finish()
+    }
 }
-    /*class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        }
-    }*/
+fun AppCompatActivity.goto(des: String?) {
+    des?.let { startActivity(Intent(des)) }
+}
+/*class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}*/
